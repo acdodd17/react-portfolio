8c08e0c0a511b3264c9a4abe8c9b732e648506df
acdodd17 <acdodd17@gmail.com>
2022-03-07 07:25:39 -0500

    resume page updates and downloadable pdf

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -6,19 +6,33 @@ function Resume() {
             </h2>
             
             <div className="row">
-                <div className="column">
+                <div className="column proficiencies">
                     <h3 className="subtitle">
                         Front-End Proficiencies 
                     </h3>
+                    <ul className="proficiencies-list">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                    </ul>
                 </div>
-                <div className="column">
+                <div className="column proficiencies">
                     <h3 className="subtitle">
                         Back-End Proficiencies 
                     </h3>
+                    <ul className="proficiencies-list">
+                        <li>Node.js</li>
+                        <li>Express</li>
+                        <li>MySQL</li>
+                        <li>MongoDB</li>
+                    </ul>
                 </div>
             </div>
             <div className="row">
-            <a href="https://github.com/acdodd17/react-portfolio/blob/main/public/Multiplication%20Monster.pdf" download="Sample" className="resume-link p__outfit-black">Click to download my resume!</a>
+                <div className="column">
+                    <a href="Resume.pdf" download="Sample" className="resume-link p__outfit-black flex__center">Click to download my resume!</a>
+                </div>
             </div>
         </div>
     )
