06fb86b0df4f69aa2602364c0330f29dcffbb2fe
acdodd17 <acdodd17@gmail.com>
2022-03-08 09:33:39 -0500

    trying to fix image errors on deploy

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
-import _0 from '../../assets/images/_0.png';
-import _1 from '../../assets/images/_1.png';
-import _2 from '../../assets/images/_2.png';
-import _3 from '../../assets/images/_3.png';
-import _4 from '../../assets/images/_4.png';
-import _5 from '../../assets/images/_5.png';
+import p0 from '../../assets/images/p0.png';
+import p1 from '../../assets/images/p1.png';
+import p2 from '../../assets/images/p2.png';
+import p3 from '../../assets/images/p3.png';
+import p4 from '../../assets/images/p4.png';
+import p5 from '../../assets/images/p5.png';
 
 function Project() {
     
@@ -14,37 +14,37 @@ function Project() {
             title: 'Wine Share',
             deployedURL: 'https://wine-share.herokuapp.com/', 
             github: 'https://github.com/acdodd17/wine-share', 
-            img: _0
+            img: p0
         }, 
         {
             title: "Hangman's Revenge",
             deployedURL: 'https://cyanidethejuggla.github.io/hangmans-revenge/', 
             github: 'https://github.com/CyanideTheJuggla/hangmans-revenge',
-            img: _1
+            img: p1
         }, 
         {
             title: 'Tech Talk',
             deployedURL: 'https://tech-talk-cms-style-blog.herokuapp.com/', 
             github: 'https://github.com/acdodd17/tech-talk-CMS-style-blog',
-            img: _5
+            img: p5
         },
         {
             title: 'Weather Dashboard',
             deployedURL: 'https://acdodd17.github.io/weather-dashboard/', 
             github: 'https://github.com/acdodd17/weather-dashboard',
-            img: _2
+            img: p2
         }, 
         {
             title: 'Workday Scheduler',
             deployedURL: 'https://acdodd17.github.io/work-day-schedule/', 
             github: 'https://github.com/acdodd17/work-day-schedule',
-            img: _3
+            img: p3
         }, 
         {
             title: 'Password Generator',
             deployedURL: 'https://acdodd17.github.io/password-generator/', 
             github: 'https://github.com/acdodd17/password-generator',
-            img: _4
+            img: p4
         }, 
     ];
 
