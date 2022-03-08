9704580e0eda149eceaeb8bcd004e6981ce4e424
acdodd17 <acdodd17@gmail.com>
2022-03-07 21:47:08 -0500

    image updates

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,5 +1,11 @@
 import React from "react";
-
+import ProjectItem from "./ProjectItem";
+import _0 from '../../assets/images/_0.png';
+import _1 from '../../assets/images/_1.png';
+import _2 from '../../assets/images/_2.png';
+import _3 from '../../assets/images/_3.png';
+import _4 from '../../assets/images/_4.png';
+import _5 from '../../assets/images/_5.png';
 
 function Project() {
     
@@ -7,32 +13,38 @@ function Project() {
         {
             title: 'Wine Share',
             deployedURL: 'https://wine-share.herokuapp.com/', 
-            github: 'https://github.com/acdodd17/wine-share'
+            github: 'https://github.com/acdodd17/wine-share', 
+            img: _0
         }, 
         {
             title: "Hangman's Revenge",
             deployedURL: 'https://cyanidethejuggla.github.io/hangmans-revenge/', 
-            github: 'https://github.com/CyanideTheJuggla/hangmans-revenge'
+            github: 'https://github.com/CyanideTheJuggla/hangmans-revenge',
+            img: _1
         }, 
         {
             title: 'Tech Talk',
             deployedURL: 'https://tech-talk-cms-style-blog.herokuapp.com/', 
-            github: 'https://github.com/acdodd17/tech-talk-CMS-style-blog'
+            github: 'https://github.com/acdodd17/tech-talk-CMS-style-blog',
+            img: _5
         },
         {
             title: 'Weather Dashboard',
             deployedURL: 'https://acdodd17.github.io/weather-dashboard/', 
-            github: 'https://github.com/acdodd17/weather-dashboard'
+            github: 'https://github.com/acdodd17/weather-dashboard',
+            img: _2
         }, 
         {
             title: 'Workday Scheduler',
             deployedURL: 'https://acdodd17.github.io/work-day-schedule/', 
-            github: 'https://github.com/acdodd17/work-day-schedule'
+            github: 'https://github.com/acdodd17/work-day-schedule',
+            img: _3
         }, 
         {
             title: 'Password Generator',
             deployedURL: 'https://acdodd17.github.io/password-generator/', 
-            github: 'https://github.com/acdodd17/password-generator'
+            github: 'https://github.com/acdodd17/password-generator',
+            img: _4
         }, 
     ];
 
@@ -40,23 +52,7 @@ function Project() {
         <div className="portfolio row">
             {projects.map((project, i) => (
                 
-                <div className="column portfolio-project">
-                    <div  className="project" >
-                        <a href={project.deployedURL} target="_blank">
-                            <img src={require(`../../assets/images/_${i}.png`)} 
-                            alt={project.title} 
-                            className="project-img" 
-                            key={project.title}/>
-                        </a>
-                        <h2 className="project-title p__outfit-black subheading flex__center">{project.title}</h2>
-                        <div className="project-links">
-                            <a href={project.github} target="_blank"><img src="https://img.icons8.com/ios/50/000000/github--v2.png" 
-                            alt="Github Repository" id="project-icon"/></a>
-                            <a href={project.deployedURL} target="_blank"><img src="https://img.icons8.com/ios-filled/50/000000/link--v2.png" 
-                            alt="Project URL" id="project-icon"/></a>
-                        </div>
-                    </div>
-                </div>
+                <ProjectItem project = {project} key = {project.title}/>
                 
             ))}
         </div>
